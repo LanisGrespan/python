ec845dfafb60750a1e14a487c0304eb36abca66c
LanisSenai <151063591+LanisSenai@users.noreply.github.com>
2024-03-26 15:18:50 -0300

    Add files via upload

---
+++ projeto/database.py
@@ -0,0 +1,20 @@
+Equipamentos = [{"id": 1, "classificacao": "Computador", "modelo": "Apple", "valor": 5990, "id_sala":22}
+                ,{"id": 2, "classificacao": "Computador", "modelo": "Lenovo", "valor": 2551.90, "id_sala":22}
+                ,{"id": 3, "classificacao": "Computador", "modelo": "Acer", "valor":2663.10, "id_sala":22}
+                ,{"id": 4, "classificacao": "Notebook", "modelo": "Dell", "valor":4998, "id_sala":22}
+                ,{"id": 5, "classificacao": "Notebook", "modelo": "Dell", "valor":4998, "id_sala":22}
+                ,{"id": 6, "classificacao": "Smartphone", "modelo": "Apple", "valor": 13548, "id_sala":21}
+                ,{"id": 7, "classificacao": "Smartphone", "modelo": "Samsung", "valor": 4998, "id_sala":21}
+                ,{"id": 8, "classificacao": "Smartphone", "modelo": "Xiaomi", "valor":2663.10, "id_sala":20}
+                ,{"id": 9, "classificacao": "Smartphone", "modelo": "Motorola", "valor":2551.90, "id_sala":20}
+                ,{"id": 10, "classificacao": "Sofá", "modelo": "Retrátil", "valor": 5990, "id_sala":22}
+                ,{"id": 11, "classificacao": "Sofá", "modelo": "Lateral", "valor": 2551.90, "id_sala":21}
+                ,{"id": 12, "classificacao": "Mesa", "modelo": "Dobravel", "valor":2663.10, "id_sala":21}
+                ,{"id": 13, "classificacao": "Mesa", "modelo": "De centro", "valor":4998, "id_sala":22}
+                ]
+
+Salas = [{"id_sala": 20, "nome": "Laboratório"}
+        ,{"id_sala": 21, "nome": "Física"}
+        ,{"id_sala": 22, "nome": "Informática"}
+]
+
